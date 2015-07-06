a227e0949d807f4815a86c5d177ef9db612f2e47
Andrew Walz <andrew@bananasplit.io>
2015-07-06 13:06:01 -0600

    Initial Commit

---
+++ OpenSketch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenSketch
+//
+//  Created by Andrew on 2015-07-06.
+//  Copyright © 2015 Cashew Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
