f69001e1eb784e07e3a16f9aea40b81b6c21641c
Andrew Walz <andrew@bananasplit.io>
2015-07-06 18:38:33 -0600

    Added outline to UIButtons and subclassed colorPaletteButton

--- OpenSketch/ColorPaletteButton.swift
+++ OpenSketch/ColorPaletteButton.swift
@@ -9,55 +9,84 @@
 import UIKit
 
 class ColorPaletteButton: UIButton {
+    
+    var fillColor = UIColor.whiteColor().CGColor
+    
+    override func drawRect(rect:CGRect)
+        
+    {
+        // obtain context
+        let context = UIGraphicsGetCurrentContext()
+        
+        // decide on radius
+        let rad = CGRectGetWidth(rect)/2-2
+        
+        let endAngle = CGFloat(2*M_PI)
+        
+        // add the circle to the context
+        CGContextAddArc(context, CGRectGetMidX(rect), CGRectGetMidY(rect), rad, 0, endAngle, 1)
+        
+        // set fill color
+        CGContextSetFillColorWithColor(context,fillColor)
+        
+        // set stroke color
+        CGContextSetStrokeColorWithColor(context,UIColor.blackColor().CGColor)
+        
+        // set line width
+        CGContextSetLineWidth(context, 3.0)
+        // use to fill and stroke path (see http://stackoverflow.com/questions/13526046/cant-stroke-path-after-filling-it )
+        
+        // draw the path
+        CGContextDrawPath(context, kCGPathFillStroke);
+    }
 
+    
 }
 
 class RedButton: ColorPaletteButton {
+    
     override func drawRect(rect: CGRect) {
-        let path = UIBezierPath(ovalInRect: rect)
-        UIColor.redColor().setFill()
-        path.fill()
+        fillColor = UIColor.redColor().CGColor
+        super.drawRect(rect)
     }
 }
 
 class BlueButton: ColorPaletteButton {
+    
     override func drawRect(rect: CGRect) {
-        let path = UIBezierPath(ovalInRect: rect)
-        UIColor.blueColor().setFill()
-        path.fill()
+        fillColor = UIColor.blueColor().CGColor
+        super.drawRect(rect)
     }
 }
 
 class GreenButton: ColorPaletteButton {
+    
     override func drawRect(rect: CGRect) {
-        let path = UIBezierPath(ovalInRect: rect)
-        UIColor.greenColor().setFill()
-        path.fill()
+        fillColor = UIColor.greenColor().CGColor
+        super.drawRect(rect)
     }
 }
 
 class YellowButton: ColorPaletteButton {
+    
     override func drawRect(rect: CGRect) {
-        let path = UIBezierPath(ovalInRect: rect)
-        UIColor.yellowColor().setFill()
-        path.fill()
+        fillColor = UIColor.yellowColor().CGColor
+        super.drawRect(rect)
     }
 }
 
 class PurpleButton: ColorPaletteButton {
+    
     override func drawRect(rect: CGRect) {
-        let path = UIBezierPath(ovalInRect: rect)
-        UIColor.purpleColor().setFill()
-        path.fill()
+        fillColor = UIColor.purpleColor().CGColor
+        super.drawRect(rect)
     }
 }
 
 class OrangeButton: ColorPaletteButton {
+    
     override func drawRect(rect: CGRect) {
-        let path = UIBezierPath(ovalInRect: rect)
-        UIColor.orangeColor().setFill()
-        path.fill()
+        fillColor = UIColor.orangeColor().CGColor
+        super.drawRect(rect)
     }
 }
-
-
